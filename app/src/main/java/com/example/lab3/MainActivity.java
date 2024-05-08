d72a3c24e764e4b7495df74eea30b9bcb5c253cb
NikolaiShinkorenko <160001185+NikolaiShinkorenko@users.noreply.github.com>
2024-05-08 16:15:56 +0600

    Button handler in Mainactivity

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lab3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button linerButton = (Button) findViewById(R.id.liner_button);
+        Button constraintButton = (Button) findViewById(R.id.constraint_button);
+        Button frameButton = (Button) findViewById(R.id.frame_button);
+
+        linerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinerLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+        constraintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConstraintLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+        frameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FrameLayoutActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
